8c21bd9f6f48f2e3e54996594cd722a75cea803d
Raisan <ytpc26ms@outlook.jp>
2021-09-01 08:51:30 -0700

    Create install.ps1

---
+++ script/1.2.4/install.ps1
@@ -0,0 +1,13 @@
+Write-Host "Download Floorp_Browser_v1.2.4 installer. Is it OK?"
+$answer = Read-Host "[Y/n]"
+if ( "y" -eq $answer){
+    $cli = New-Object System.Net.WebClient
+    $uri = New-Object System.Uri("https://repo.ablaze.one/floorp/1.2.4/Floorp_Browser_setup.exe")
+    $file = Split-Path $uri.AbsolutePath -Leaf
+    $cli.DownloadFile($uri, (Join-Path "./" $file))
+    Write-Host "Downloading is complete!" -ForegroundColor Cyan
+    start Floorp_Browser_setup.exe
+    Write-Host "Start installer!" -ForegroundColor Cyan
+}else{
+    Write-Host "Canceled"
+}
